91f4ba9866905875f1c0eb4053e7bd2b1dec3808
Mayank <mayankmchandratre@gmail.com>
2024-07-25 13:58:14 +0530

    3rd try

--- apps/user-app/provider.tsx
+++ apps/user-app/provider.tsx
@@ -3,6 +3,7 @@ import { RecoilRoot } from "recoil";
 import { SessionProvider } from "next-auth/react";
 //comment 4 cloud updated key
 export const Providers = ({children}: {children: React.ReactNode}) => {
+    //comment1
     return <RecoilRoot>
         <SessionProvider>
             {children}
